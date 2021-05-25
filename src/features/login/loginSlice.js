f8b256f8d8e8aa99cfe09805875f39bda6d45bc8
braulioo94 <brauliot.salazar@hotmail.com>
2021-05-25 18:47:41 -0300

    Se agrega el reducer y actions para loguearse, desloguearse y guardar informacion del usuario en el state

---
+++ src/features/login/loginSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState: {
+    userActive: false,
+    nombreUsuario:null
+  },
+  reducers: {
+    logIn: (state ) => {
+        state.userActive = true
+        }, 
+        
+    guardarNombre: (state, action) => {
+        state.nombreUsuario = action.payload
+        },     
+    
+    logOut: (state) => (
+        state.userActive = false,
+        state.nombreUsuario = null
+    )
+    
+  },
+})
+
+
+export const { logIn, logOut,guardarNombre} = loginSlice.actions
+
+export default loginSlice.reducer
