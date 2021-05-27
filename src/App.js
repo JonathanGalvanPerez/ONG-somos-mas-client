c149b4554b6e9e3e22c8f1be0fd866ed09a2e5dd
Alejandro Sosa <agsosa74@gmail.com>
2021-05-27 13:24:56 -0300

    Init LoginPage/Form

--- src/App.js
+++ src/App.js
@@ -6,29 +6,31 @@ import Actividades from './components/Actividades';
 import Contribuye from './components/Contribuye';
 import { useDispatch } from 'react-redux';
 import { fetchPublicInfoData } from './app/publicInfoSlice';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   const dispatch = useDispatch();
- 
+
   // Fetch organization's public info data as soon as we load the app
   React.useEffect(() => {
-    dispatch(fetchPublicInfoData())
+    dispatch(fetchPublicInfoData());
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/inicio" />
-          </Route>
-          <Route exact path="/inicio" component={Inicio} />
-          <Route exact path="/nosotros" component={Nosotros} />
-          <Route exact path="/actividades" component={Actividades} />
-          <Route exact path="/contribuye" component={Contribuye} />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/inicio' />
+        </Route>
+        <Route exact path='/inicio' component={Inicio} />
+        <Route exact path='/nosotros' component={Nosotros} />
+        <Route exact path='/actividades' component={Actividades} />
+        <Route exact path='/contribuye' component={Contribuye} />
+        <Route exact path='/acceso' component={LoginPage} />
+        <Route exact path='/registro' component={RegisterPage} />
+      </Switch>
+    </Router>
   );
 }
 
