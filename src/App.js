a15f3f70d1a3606a224c6a9c93c7299d006196c5
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-05-28 01:26:52 -0300

    display Loader on loading state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Inicio from './components/Inicio';
 import Nosotros from './components/Nosotros';
 import Actividades from './components/Actividades';
 import Contribuye from './components/Contribuye';
-import { useDispatch } from 'react-redux';
-import { fetchPublicInfoData } from './app/publicInfoSlice';
+import Loader from './components/Loading/Loader'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPublicInfoData, publicLoading } from './app/publicInfoSlice';
 
 function App() {
+
   const dispatch = useDispatch();
- 
   // Fetch organization's public info data as soon as we load the app
   React.useEffect(() => {
     dispatch(fetchPublicInfoData())
   }, [dispatch]);
+  
+  // get loading state. repeat for every slices
+  const publicloading = useSelector(publicLoading);
+  const loading = (publicloading === "pending");
 
   return (
     <div className="App">
+      <Loader isLoading={loading} />
       <Router>
         <Switch>
           <Route exact path="/">
