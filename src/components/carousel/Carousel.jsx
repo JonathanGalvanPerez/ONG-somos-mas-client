57071ce97d0205a67f266a3c809a454e3a516c34
Fabo <heckiz@Heckiz.dev>
2021-05-29 02:37:58 -0300

    add dynamic loading and style adjustments

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,94 +1,122 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+
+//test images
+import Slide1 from "../../assets/images/slide3.jpg";
+import Slide2 from "../../assets/images/slide1.jpg";
+import Slide3 from "../../assets/images/slide2.jpg";
 
 export default function Carousel() {
 
-    const [items, setItems] = useState(
+    const [data, setData] = useState(
         {
             logoColors: ['#DB5752', '#FAFA88', '#9AC9FB'],
-            data: [
-                {
-                    imageUrl: 'https://www.latercera.com/resizer/mI8vn88DUIOFUemIM9TSEyr0FDo=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/6OCZCBLAE5AEXADOCEPV4D5HG4.jpg',
-                    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi optio laborum itaque architecto eum porro aspernatur. Tempora consequatur laborum nostrum impedit, odio non perspiciatis quisquam debitis numquam quibusdam libero eaque?"
-                },
-                {
-                    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdqUuLekYoxIRK4lcfXqxbHeTqXOgZkZIeZQ&usqp=CAU',
-                    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui id error cum maxime suscipit debitis rerum, quo omnis quam, perspiciatis explicabo, quia magnam illo quibusdam facilis dolores quis in!"
-                },
-                {
-                    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDYVnb5eydDWpCbtM-YyDje_7MjAbrcSZ5XYlfy1UF056HyILE2g9QjPrv7k4IQaKKubg&usqp=CAU',
-                    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas reiciendis exp'
-                }
-            ],
-            responsive: {
+            responsiveBreakpoints: {
                 0: {
                     items: 1,
                 },
-                600: {
+                768: {
                     items: 2,
                 },
-                1000: {
+                992: {
                     items: 3,
                 }
-            }
+            },
+            items: [],
+
         }
     )
 
+    useEffect(() => {
+        return setData({
+            ...data, items: [
+                // <test data>
+                {
+                    imageUrl: Slide1,
+                    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi optio laborum itaque architecto eum porro aspernatur. "
+                },
+                {
+                    imageUrl: Slide2,
+                    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui id error cum maxime suscipit debitis rerum, quo omnis quam, perspiciatis explicabo, quia magnam illo quibusdam facilis dolores quis in!"
+                },
+                {
+                    imageUrl: Slide3,
+                    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas reiciendis exp'
+                }
+                // </test data> 
+            ]
+        })
+
+    }, [])
 
     return (
         <div>
             <OwlCarousel
                 className='owl-theme'
                 loop margin={5}
-                responsive={items.responsive}
+                responsive={data.responsiveBreakpoints}
             >
                 {
-                    items.data.map(({ imageUrl, text }, index) => {
+                    data.items.map(({ imageUrl, text }, index) => {
                         return (
-                            <>
-                                <Box
-                                    transitionProperty="all"
-                                    transitionDuration="500ms"
-                                    boxShadow="2xl" rounded="md"
-                                    _hover={{ transform: 'scale(1.03)' }}
+
+                            <Box
+                                cursor='pointer'
+                                key={index}
+                                transitionProperty="all"
+                                transitionDuration="500ms"
+                                boxShadow="2xl" rounded="md"
+                                _hover={{
+                                    transform: 'scale(1.03)',
+                                    '.textBox': {
+                                        height: 'max-content',
+                                        opacity: '1',
+                                        visibility: 'visible'
+                                    }
+                                }}
+
+                            >
+                                <Box backgroundImage={imageUrl} backgroundSize="cover" height="35vh" />
+                                <Box className="textBox"
+                                    bg={data.logoColors[index]}
+                                    opacity="0"
+                                    visibility="hidden"
+                                    height="8vh"
+                                    p="2"
+                                    overflow="hidden"
+                                    rounded="sm"
+                                    transition='visibility 0s, opacity 500ms linear'
+
                                 >
 
-                                    <Image src={imageUrl} alt="" height="35vh" />
-
                                     <Box
-                                        bg={items.logoColors[index]}
-                                        height="7vh"
-                                        p="2"
-                                        _hover={{ height: 'max-content' }}
+                                        rounded="md"
+                                        opacity="95%"
+                                        bg="gray.800"
+                                        paddingY="2"
+                                        paddingX="5"
+
                                     >
 
-                                        <Box
-                                            rounded="md"
-                                            opacity="95%"
-                                            bg="gray.800"
-                                            paddingY="1"
-                                            paddingX="5"
+                                        <Text
+                                            as="cite"
+                                            fontSize="lg"
+                                            color="white"
+
                                         >
+                                            {text}
 
-                                            <Text
-                                                as="cite"
-                                                fontSize="lg"
-                                                color="white"
-                                                lineHeight="200%"
-                                            >
-                                                {text}
-
-                                            </Text>
-
-                                        </Box>
+                                        </Text>
 
                                     </Box>
 
                                 </Box>
-                            </>
+
+                            </Box>
+
                         )
                     })
                 }
