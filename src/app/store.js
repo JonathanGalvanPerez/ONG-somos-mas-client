4b8f429d858a792de143342385355e8c8413abb2
Braulio Salazar <brauliot.salazar@hotmail.com>
2021-05-30 14:50:08 -0300

    Added footer, and functionality to delete and edit buttons

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import publicInfoReducer from './publicInfoSlice';
+import newsDeleteReducer from '../features/news/newsDeleteSlice'
+import newsPutReducer from '../features/news/newsPutSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    publicInfo: publicInfoReducer
+    publicInfo: publicInfoReducer,
+    newsDelete: newsDeleteReducer,
+    newsPut:newsPutReducer
   },
 });
