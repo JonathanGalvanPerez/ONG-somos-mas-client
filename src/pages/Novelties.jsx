92346efa32bf1de0664b045d03652b86efd2fe1b
Fabo <heckiz@Heckiz.dev>
2021-05-31 00:38:19 -0300

    styles retouching

--- src/pages/Novelties.jsx
+++ src/pages/Novelties.jsx
@@ -1,10 +1,8 @@
 import moment from 'moment'
 import { Link } from 'react-router-dom';
 import { Box, Grid, GridItem, Text } from '@chakra-ui/layout'
-import React, { useState } from 'react'
+import React from 'react'
 import Card from '../components/novelties/Card'
-
-
 
 // test data
 import Image1 from "../assets/images/novedad1.jpg";
@@ -14,25 +12,25 @@ import Image4 from "../assets/images/novedad4.jpg";
 
 const testData = [
   {
-    id:'1',
+    id: '1',
     title: 'new one',
     image: Image1,
     createAd: Date.now()
   },
   {
-    id:'2',
+    id: '2',
     title: 'new two',
     image: Image2,
     createAd: Date.now()
   },
   {
-    id:'3',
+    id: '3',
     title: 'new three',
     image: Image3,
     createAd: Date.now()
   },
   {
-    id:'4',
+    id: '4',
     title: 'new four',
     image: Image4,
     createAd: Date.now()
@@ -42,41 +40,54 @@ const testData = [
 
 export default function Novelties() {
 
-  // const [data, setData] = useState({})
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //     (async () => {
+  //         await dispatch(getEntries());
+  //     })();
+  // }, [dispatch])
+
+  // const allEntries = useSelector(state => state.state);
 
   return (
     <Box display="flex" justifyContent="center">
 
-      <Box bg="blue.100" w={{ base: "100%", lg: "80%" }} h="max-content">
+      <Box
+        bg="green.100" h="max-content"
+        w={{ base: "100%", lg: "80%" }} >
 
         <Box bg="green.200" textAlign="center">
+
           <Text fontSize="5xl" as="em">
             Novedades
           </Text>
-
+          
         </Box>
+
+
+
         <Grid
           templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(2, 1fr)" }}
           gap={6}
-          p={{base:"5", lg:"10"}}
+          p={{ base: "5", lg: "10" }}
         >
           {
             testData.map((item, index) => (
 
 
               <Link to={`/novedades/${item.id}`}>
-              
-              <GridItem colSpan={1} cursor="pointer" borderRadius="lg" _hover={{
-                transform: 'scale(1.05)', transitionProperty: 'all',
-                transitionDuration: '0.5s',
-                borderWidth: "4px", borderColor: "blue.300"
-              }}>
-                <Card imageUrl={item.image} index={index} createAt={ moment(item.createAd).format('LL')} title={item.title} />
-              </GridItem>
-              
+
+                <GridItem colSpan={1} cursor="pointer" borderRadius="lg" _hover={{
+                  transform: 'scale(1.05)', transitionProperty: 'all',
+                  transitionDuration: '0.5s',
+                  borderWidth: "4px", borderColor: "teal.300"
+                }}>
+                  <Card imageUrl={item.image} index={index} createAt={moment(item.createAd).format('LL')} title={item.title} />
+                </GridItem>
+
               </Link>
 
-              
+
             ))
           }
 
