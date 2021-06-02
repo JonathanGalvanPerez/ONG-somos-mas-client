e276b723c91986a0566aa16b7d41398936fa6a50
Brian Retamar <leo1632@hotmail.com>
2021-06-02 20:47:20 -0300

    Added contact & profile pages to Router

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import Footer from "./components/layout/footer";
 import Header from "./components/layout/header";
 import Backoffice from "./components/Backoffice";
 import EditHomeForm from "./components/EditHomeForm";
+import Contacto from './components/Contact/index'
+import Profile from './components/Profile/index'
 
 function App() {
   const dispatch = useDispatch();
@@ -47,6 +49,8 @@ function App() {
           <Route exact path="/registro" component={RegisterPage} />
           <Route exact path="/editar-inicio" component={EditHomeForm} />
           <Route exact path="/backoffice" component={Backoffice} />
+          <Route exact path="/contacto" component={Contacto} />
+          <Route exact path="/perfil" component={Profile} />
         </Switch>
       </Router>
       <Footer />
