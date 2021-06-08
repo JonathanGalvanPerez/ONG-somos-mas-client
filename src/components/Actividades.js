a72b50462b4269a77f0480199839610f2a98b3cd
Fabo <heckiz@Heckiz.dev>
2021-06-08 13:36:48 -0300

    remove test lines

--- src/components/Actividades.js
+++ src/components/Actividades.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import CardDetail from './novelties/CardDetail'
 
 export default class Actividades extends Component {
     render() {
         return (
             <div>
-                {/* Test Novelties Detail */}
-                <CardDetail />
+                Seccion de actividades
             </div>
         )
     }
