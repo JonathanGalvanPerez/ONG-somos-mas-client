329429d97e8254661535e9f8752192ffbd996ce9
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-09 05:45:01 -0300

    now token and role are stored before app refresh

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import publicInfoReducer from './publicInfoSlice';
-import loginReducer from '../features/login/loginSlice'
-import usersInfoReducer from "./usersInfoSlice"
-import activityReducer from '../features/activities/activitySlice'
+import loginReducer from '../features/login/loginSlice';
+import usersInfoReducer from "./usersInfoSlice";
+import activityReducer from '../features/activities/activitySlice';
+import newsDeleteReducer from '../features/news/newsDeleteSlice';
+import newsPutReducer from '../features/news/newsPutSlice';
 
 const preloadedState = {
   login: {
     token: JSON.parse(localStorage.getItem('org_token')),
+    roleId: JSON.parse(localStorage.getItem('userRole'))
   }
 };
 
@@ -18,16 +21,17 @@ const store = configureStore({
     login: loginReducer,
     activity:activityReducer,
     newsDelete: newsDeleteReducer,
-    newsPut:newsPutReducer,
-    usersInfo: usersInfoReducer,
-    activity:activityReducer
+    newsPut: newsPutReducer,
+    usersInfo: usersInfoReducer
     
   },
   preloadedState,
 });
 
-store.subscribe(()=>{
-  localStorage.setItem('org_token', JSON.stringify(store.getState().login.token));
-})
+window.onbeforeunload = () => {
+  const state = store.getState();
+  localStorage.setItem('org_token', JSON.stringify(state.login.token));
+  localStorage.setItem('userRole', JSON.stringify(state.login.roleId));
+}
 
 export default store;
