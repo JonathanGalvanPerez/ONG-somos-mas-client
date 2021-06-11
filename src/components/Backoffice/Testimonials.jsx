07633ef0021c5724799790503d078d90e3a57547
Fabo <heckiz@Heckiz.dev>
2021-06-11 02:56:35 -0300

    add styles to components

--- src/components/Backoffice/Testimonials.jsx
+++ src/components/Backoffice/Testimonials.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
 import Card from '../testimonials/Card'
+import { Box, Grid, GridItem, Text } from '@chakra-ui/layout';
+
 export default function Testimonials() {
-    return (
-        <div>
-            Page Testimonials Backoffice
-            <Card/>
-        </div>
-    )
+  const testimonials = [
+    {
+      name: "James Cameron",
+      content:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga esse eligendi repellat modi similique reiciendis!"
+    },
+    {
+      name: "Tim Burton",
+      content:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga esse eligendi repellat modi similique reiciendis!"
+    },{
+      name: "Quentin Tarantino",
+      content:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga esse eligendi repellat modi similique reiciendis!"
+    },{
+      name: "Steven Spielberg",
+      content:" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga esse eligendi repellat modi similique reiciendis!"
+    }    
+  ]
+  return (
+    <Box display="flex" justifyContent="center" p="2px">
+
+      <Box
+        bg="blue.100" h="max-content"
+       
+        w={{ base: "100%", lg: "80%" }} >
+
+        <Box bg="#9AC9FB" textAlign="center">
+
+          <Text 
+          color="#474645"
+          fontWeight="bold"
+          as="i"
+          fontSize={{ base: "3xl", md:"4xl", lg: "5xl" }}
+          textTransform="uppercase"
+          textShadow="2px 2px white"
+           >
+            Testimonials
+          </Text>
+
+        </Box>
+
+
+
+        <Grid
+          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(2, 1fr)" }}
+          gap={6}
+          p={{ base: "5", lg: "10" }}
+        >
+          {
+            testimonials.map(({ name, content }) => (
+
+
+                <GridItem colSpan={1} cursor="pointer">
+                  <Card name={name} content={content}  />
+                </GridItem>
+
+
+
+            ))
+          }
+
+        </Grid>
+      </Box>
+    </Box>
+  )
 }
--- src/components/testimonials/Card.jsx
+++ src/components/testimonials/Card.jsx
@@ -1,9 +1,48 @@
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
+import { Box, Spacer, Text, Button, Flex } from '@chakra-ui/react'
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
     return (
-        <div>
-            Card Testimonials
-        </div>
+        <Box
+            bg="white" p="2.5"
+            dispdlay="flex"
+            borderRadius="lg"
+            boxShadow="lg"
+        >
+
+            <Text
+                p="2.5"
+                textTransform="uppercase"
+                fontSize="2xl"
+                textShadow="-2px 0px 2px #FAFA88"
+                fontWeight="semibold"
+            >
+
+                {props.name}
+
+            </Text>
+
+            <Spacer />
+
+            <Box
+                px="3" py="2"
+                borderLeft="2px"
+                borderLeftColor="#DB5752"
+            >
+
+                <Text
+                    as="cite"
+                    fontSize="lg"
+                >
+
+                    <q>{props.content}</q>
+
+
+                </Text>
+            </Box>
+
+
+        </Box>
     )
 }
