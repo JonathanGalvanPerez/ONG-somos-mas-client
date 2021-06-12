e04af82a85ca8a36e1bf00b9a8cab05e73c4681b
Alejandro Sosa <agsosa74@gmail.com>
2021-06-12 02:50:21 -0300

    Modular component

--- src/components/Contact/ContactoMsg.jsx
+++ src/components/Contact/ContactoMsg.jsx
@@ -7,8 +7,68 @@ import { ImInstagram } from 'react-icons/im';
 import { SimpleGrid } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 
+const AboutUs = (
+  <>
+    <Text>
+      Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las familias
+      del barrio.
+    </Text>
+    <Text>
+      Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de trabajo.
+    </Text>
+    <Text>
+      Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: Educación, deportes,
+      primera infancia, salud, alimentación y trabajo social.
+    </Text>
+  </>
+);
+
 export default function ContactoMsg() {
   const { data } = useSelector((state) => state.publicInfo);
+
+  const socialLinks = [
+    {
+      link: `https://www.facebook.com/${data.orgContact[0].facebook}`,
+      icon: <GrFacebook />,
+      value: data.orgContact[0].facebook,
+      default: 'Somos_Más',
+      color: 'blue.600',
+    },
+    {
+      link: `https://www.instagram.com/${data.orgContact[0].instagram}`,
+      icon: <ImInstagram />,
+      value: data.orgContact[0].instagram,
+      default: 'SomosMás',
+      color: '#E83C78',
+    },
+    {
+      link: `https://www.twitter.com/${data.orgContact[0].twitter}`,
+      icon: <FaTwitterSquare />,
+      value: data.orgContact[0].twitter,
+      default: 'SomosMás',
+      color: 'cyan.500',
+    },
+    {
+      link: `tel:${data.phone}`,
+      icon: <FaPhoneSquareAlt />,
+      value: data.phone,
+      default: '1160112988',
+      color: 'green.400',
+    },
+  ];
+
+  const SocialLinks = (
+    <SimpleGrid spacing={5} columns='2' w={{ base: '95%', md: '75%' }}>
+      {socialLinks.map((social) => (
+        <HStack key={social.label} color={social.color} fontSize='lg' justify='center'>
+          {social.icon}
+          <Link isExternal={social.value != null} href={`${social.value ? social.link : '#'}`}>
+            {social.value || social.default}
+          </Link>
+        </HStack>
+      ))}
+    </SimpleGrid>
+  );
 
   return (
     <VStack border='gray.100 solid 1px' pr={5}>
@@ -19,41 +79,23 @@ export default function ContactoMsg() {
               Contacto
             </Box>
             <VStack spacing={5} letterSpacing='wide' color='gray.800'>
-              <Text>
-                Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las
-                familias del barrio.
-              </Text>
-              <Text>
-                Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de
-                trabajo.
-              </Text>
-              <Text>
-                Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: Educación,
-                deportes, primera infancia, salud, alimentación y trabajo social.
-              </Text>
+              {AboutUs}
             </VStack>
           </Stack>
-          <HStack fontSize={{base:"md", sm: 'lg'}} justify='center' mb={5} mt={6} borderTop='1px' borderColor='gray.200' pt={5}>
+          <HStack
+            fontSize={{ base: 'md', sm: 'lg' }}
+            justify='center'
+            mb={5}
+            mt={6}
+            borderTop='1px'
+            borderColor='gray.200'
+            pt={5}>
             <MdEmail />
-            <Link to={`mailto:${data.orgContact[0].email || 'somosfundacionmas@gmail.com'}`}>
+            <Link isExternal href={`mailto:${data.orgContact[0].email || 'somosfundacionmas@gmail.com'}`}>
               {data.orgContact[0].email || 'somosfundacionmas@gmail.com'}
             </Link>
           </HStack>
-          <SimpleGrid spacing={5} columns="2" w={{base:"95%", md:'75%'}}>
-            <HStack color='blue.600' fontSize='lg' justify='center'>
-              <GrFacebook />
-              <Link>{data.orgContact[0].facebook || 'Somos_Más'}</Link>
-            </HStack>
-            <HStack color='#E83C78' fontSize='lg' justify='center'>
-              <ImInstagram /> <Link>{data.orgContact[0].instagram || 'SomosMás'}</Link>
-            </HStack>
-            <HStack color='cyan.500' fontSize='lg' justify='center'>
-              <FaTwitterSquare /> <Link>{data.orgContact[0].twitter || 'SomosMás'}</Link>
-            </HStack>
-            <HStack color='green.400' fontSize='lg' justify='center'>
-              <FaPhoneSquareAlt /> <Link>{data.phone || '1160112988'}</Link>
-            </HStack>
-          </SimpleGrid>
+          {SocialLinks}
         </Container>
       </Box>
     </VStack>
