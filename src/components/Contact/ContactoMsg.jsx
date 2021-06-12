39d27f4280ce4e92ee567755b63ea73e8c1f6fd6
Alejandro Sosa <agsosa74@gmail.com>
2021-06-12 00:36:42 -0300

    Add social icons grid

--- src/components/Contact/ContactoMsg.jsx
+++ src/components/Contact/ContactoMsg.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Box, Container, Link, Stack, Text, VStack, List, ListItem, ListIcon } from '@chakra-ui/layout';
+import { Box, Container, Link, Stack, Text, VStack, List, ListItem, ListIcon, Grid, HStack } from '@chakra-ui/layout';
 import { AccordionItem, AccordionButton, Accordion, AccordionIcon, AccordionPanel } from '@chakra-ui/accordion';
 import { MdCheckCircle, MdSettings } from 'react-icons/md';
 import { GrFacebook } from 'react-icons/gr';
+import { SimpleGrid } from '@chakra-ui/react';
 
 export default function ContactoMsg() {
   return (
     <VStack border='gray.100 solid 1px'>
       <Box flexShrink={0} marginX='6' mb={5}>
         <Container centerContent>
-          <Stack spacing={5}>
-            <Text fontSize='3xl' align='center' mt={4} fontWeight='bold'>
+          <Stack spacing={10}>
+            <Text fontSize='3xl' align='center' fontWeight='bold'>
               Contacto
             </Text>
             <VStack spacing={5}>
@@ -27,27 +28,22 @@ export default function ContactoMsg() {
                 deportes, primera infancia, salud, alimentación y trabajo social.
               </Text>
             </VStack>
-            <Box textAlign='left'>
-              <List spacing={3}>
-                <ListItem>
-                  <ListIcon as={GrFacebook} />
-                  Somos_Mas
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={MdCheckCircle} />
-                  Assumenda, quia temporibus eveniet a libero incidunt suscipit
-                </ListItem>
-                <ListItem>
-                  <ListIcon as={MdCheckCircle} />
-                  Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                </ListItem>
-                {/* You can also use custom icons from react-icons */}
-                <ListItem>
-                  <ListIcon as={MdSettings} />
-                  Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                </ListItem>
-              </List>
-            </Box>
+            <SimpleGrid spacing={3} columns={2}>
+              <HStack justify="center">
+                <GrFacebook />
+                <Link>Somos_Mas</Link>
+              </HStack>
+              <HStack justify="center">
+                <GrFacebook /> <Link>Somos_Mas</Link>
+              </HStack>
+              <HStack justify="center">
+                <GrFacebook /> <Link>Somos_Mas</Link>
+              </HStack>
+              {/* You can also use custom icons from react-icons */}
+              <HStack justify="center"  >
+                <GrFacebook /> <Link>Somos_Mas</Link>
+              </HStack>
+            </SimpleGrid>
           </Stack>
         </Container>
       </Box>
