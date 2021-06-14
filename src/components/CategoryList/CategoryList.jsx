f258e2b74cf05cbca38717499c30b2840d5467b4
Brian Retamar <leo1632@hotmail.com>
2021-06-14 15:16:41 -0300

    Added delete button

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getCategories } from './getCategories'
 import { Box, Center, Heading, Text } from '@chakra-ui/layout';
+import { DeleteIcon } from '@chakra-ui/icons'
+
 import {
     Table,
     Thead,
@@ -13,6 +15,11 @@ import {
 
 export default function ListaCategorias() {
     const [categories, setCategories] = useState([])
+
+    const handleDeleteButton = () => {
+        /* Mostrar alerta y mandar un DELETE al server */
+
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -30,11 +37,12 @@ export default function ListaCategorias() {
 
             <Text fontSize="3xl" align="center" mt={2} mb={2} fontWeight="bold">Categorías</Text>
             <Center>
-                <Box overflow="auto" mb={5} display={{ md: "flex" }} width="65%" rounded="md" shadow="dark-lg">
-                    <Table >
+                <Box overflow="auto" mb={5} display={{ md: "flex" }} width="35%" rounded="md" shadow="dark-lg">
+                    <Table variant="striped">
                         <Thead bgColor={"#9AC9FB"}>
                             <Tr>
                                 <Th>Nombre</Th>
+                                <Th textAlign="end">Eliminar</Th>
                             </Tr>
                         </Thead>
                         {categories.length === 0 ?
@@ -47,6 +55,7 @@ export default function ListaCategorias() {
                             {categories.length > 0 ? categories.map((category, index) =>
                                 <Tr key={index}>
                                     <Td fontWeight="bold">{category.name}</Td>
+                                    <Td textAlign="end"><DeleteIcon color="red.500" cursor="pointer" h={6} w={6} onClick={handleDeleteButton} /></Td>
                                 </Tr>)
                                 : null
                             }
