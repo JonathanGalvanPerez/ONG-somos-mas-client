0b16957ed79c28b2a3a3e24a8c4eaee888947943
Mariano Collavino <mcollavino64@gmail.com>
2021-06-14 14:15:35 -0300

    add transition between front elements

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Redirect,
   Switch,
+  useLocation,
 } from "react-router-dom";
 import Inicio from "./components/Inicio";
 import Nosotros from "./components/Nosotros";
@@ -33,6 +34,21 @@ import CardDetail from "./components/novelties/CardDetail";
 import News from "./components/News";
 import './App.css';
 
+//  OT34-104...inicio
+
+import {motion} from 'framer-motion';
+
+const Animator = ({ children }) => {
+  const location = useLocation();
+
+  return(
+    <motion.div initial={{x:-1000}} animate={{x:0}} transition={{duration:0.8}} key={location.pathname}>
+      {children}
+    </motion.div>
+  );
+};
+//OT34-104...fin
+
 
 function App() {
   const dispatch = useDispatch();
@@ -54,29 +70,30 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/inicio" />
-          </Route>
-          <Route exact path="/inicio" component={Inicio} />
-          <Route exact path="/nosotros" component={Nosotros} />
-          <Route exact path='/novedades' component={Novelties} />
-          <Route exact path='/novedades/:id' component={CardDetail} />
-          <Route exact path="/contribuye" component={Contribuye} />
-          <Route exact path="/acceso" component={LoginPage} />
-          <Route exact path="/registro" component={RegisterPage} />
-          <Route exact path="/editar-inicio" component={EditHomeForm} />
-          <Route exact path="/backoffice" component={Backoffice} />
-          <Route exact path="/backoffice/users" component={BackofficeUsers} />
-          <Route exact path="/backoffice/testimonials" component={Testimonials} />
-          <Route exact path="/backoffice/edit-organization" component={EditOrganization} />
-          <Route exact path="/contacto" component={Contacto} />
-          <Route exact path="/perfil" component={Profile} />
-          <Route exact path="/editar-usuario" component={EditUserForm} />
-          <Route exact path="/actividad/:id" component={ActividadId} />
-          <Route exact path="/actividades" component={ListaActividades} />
-          <Route exact path="/backoffice/contacts" component={ContactList} />
-          <Route exact path="/backoffice/news" component={News} />
-
+          <Animator>
+            <Route exact path="/">
+              <Redirect to="/inicio" />
+            </Route>
+            <Route exact path="/inicio" component={Inicio} />
+            <Route exact path="/nosotros" component={Nosotros} />
+            <Route exact path='/novedades' component={Novelties} />
+            <Route exact path='/novedades/:id' component={CardDetail} />
+            <Route exact path="/contribuye" component={Contribuye} />
+            <Route exact path="/acceso" component={LoginPage} />
+            <Route exact path="/registro" component={RegisterPage} />
+            <Route exact path="/editar-inicio" component={EditHomeForm} />
+            <Route exact path="/backoffice" component={Backoffice} />
+            <Route exact path="/backoffice/users" component={BackofficeUsers} />
+            <Route exact path="/backoffice/testimonials" component={Testimonials} />
+            <Route exact path="/backoffice/edit-organization" component={EditOrganization} />
+            <Route exact path="/contacto" component={Contacto} />
+            <Route exact path="/perfil" component={Profile} />
+            <Route exact path="/editar-usuario" component={EditUserForm} />
+            <Route exact path="/actividad/:id" component={ActividadId} />
+            <Route exact path="/actividades" component={ListaActividades} />
+            <Route exact path="/backoffice/contacts" component={ContactList} />
+            <Route exact path="/backoffice/news" component={News} />
+          </Animator>
         </Switch>
         <Footer />
       </Router>
