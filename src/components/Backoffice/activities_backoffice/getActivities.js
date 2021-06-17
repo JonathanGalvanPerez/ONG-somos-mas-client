8e11fd4bffea6bcd7ea467fc0d661ee22a2dfab1
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-17 02:02:10 -0300

    add id to testing data

--- src/components/Backoffice/activities_backoffice/getActivities.js
+++ src/components/Backoffice/activities_backoffice/getActivities.js
@@ -3,11 +3,11 @@ import Alert from '../../alertService/AlertService';
 import { API_BASE_URL } from './../../../app/config';
 
 const defaultData = [
-    {name: '1 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1},
-    {name: '2 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:0},
-    {name: '3 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1},
-    {name: '4 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1},
-    {name: '5 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:0}
+    {name: '1 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1, id:1},
+    {name: '2 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:0, id:2},
+    {name: '3 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1, id:3},
+    {name: '4 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:1, id:4},
+    {name: '5 Actividad de prueba', image:'https://via.placeholder.com/150', content: 'contenido de prueba' , deletedAt:0, id:5}
 ];
 
 export const getActivities = async () => {
