726164b06667c2ef15c0272308d8fffc24135749
Fabo <heckiz@Heckiz.dev>
2021-06-21 21:22:07 -0300

    create public testimonials route and component

--- src/pages/PublicPage.jsx
+++ src/pages/PublicPage.jsx
@@ -6,6 +6,7 @@ import Home from '../components/Inicio';
 import Nosotros from '../components/Nosotros';
 import Contactos from '../components/Contact';
 import Novelties from './Novelties';
+import Testimonials from './Testimonials';
 import CardDetail from '../components/novelties/CardDetail';
 import Actividades from "../components/Actividades/ListaActividades";
 import ActividadId from '../components/Actividades/ActividadId';
@@ -37,6 +38,7 @@ export default function PublicPage() {
                     <Route exact path={'/nosotros'} component={Nosotros} />
                     <Route exact path={'/contacto'} component={Contactos} />
                     <Route exact path={'/novedades'} component={Novelties} />
+                    <Route exact path={'/testimonios'} component={Testimonials} />
                     <Route exact path={'/novedades/:id'} component={CardDetail} />
                     <Route exact path={'/actividades'} component={Actividades} />
                     <Route exact path={'/actividad/:id'} component={ActividadId} />
---
+++ src/pages/Testimonials.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import { Box, Grid, GridItem, Text } from '@chakra-ui/layout';
+// import axios from 'axios';
+// import { API_BASE_URL } from '../app/config';
+
+
+export default function Novelties() {
+
+
+  
+
+  return (
+    <Box display="flex" justifyContent="center">
+        TESTIMONIALS PAGE
+    </Box>
+  )
+}
