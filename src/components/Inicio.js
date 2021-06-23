ec4190d1641d149be21dc19168116ccc19a859a5
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-23 01:57:48 -0300

    remove comments

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,33 +1,18 @@
 import React from "react";
 import UltimasNovedades from "./layout/UltimasNovedades";
-import Slider from '../components/carousel/Carousel'
-import Form from "./novelties/ModalNewsForm";
+import Slider from '../components/carousel/Carousel';
 
 import { Heading } from '@chakra-ui/react';
 
 export default function Inicio() {
 
-//test noveltie
-  //  const noveltie = {
-  //    title: 'title',
-  //    content: 'content',
-  //  category: 'category',
-  //    image: '?'
-  //  }
-
   return (
-    <div>
-      {/* Inserte aquí el header */}
-      <Slider/>
-
-      
-      <Heading textAlign="center" fontSize={{ base: "xl", md: "1xl", lg: "3xl" }} my={8} >Texto de Bienvenida </Heading> {/* //OT34-105 */}
-
+    <>
+      <Slider/>      
+      <Heading textAlign="center" fontSize={{ base: "xl", md: "1xl", lg: "3xl" }} my={8} >
+        Texto de Bienvenida
+      </Heading>
       <UltimasNovedades />
-
-      {/* Test NOVELTIES FORM FOR BACKOFFICE */}
-      {/* <Form label="Crear"/>
-      <Form label="Editar" data={noveltie} type="edit" /> */}
-    </div>
+    </>
   );
 }
